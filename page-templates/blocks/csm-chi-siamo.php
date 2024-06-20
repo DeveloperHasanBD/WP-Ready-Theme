e40a49a0e273bd06d893b8efa2cd2aec113f7350
Developer Hasan <67751842+DeveloperHasanBD@users.noreply.github.com>
2024-06-20 13:09:10 +0600

    Add files via upload

---
+++ page-templates/blocks/csm-chi-siamo.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Block template file: template-parts/blocks/csm-chi-siamo.php
+ *
+ * Chi Siamo page Block Template.
+ *
+ * @param   array $block The block settings and attributes.
+ * @param   string $content The block inner HTML (empty).
+ * @param   bool $is_preview True during AJAX preview.
+ * @param   (int|string) $post_id The post ID this block is saved to.
+ */
+
+// Create id attribute allowing for custom "anchor" value.
+$id = 'csm-chi-siamo-' . $block['id'];
+if (!empty($block['anchor'])) {
+    $id = $block['anchor'];
+}
+
+// Create class attribute allowing for custom "className" and "align" values.
+$classes = 'block-csm-chi-siamo';
+if (!empty($block['className'])) {
+    $classes .= ' ' . $block['className'];
+}
+if (!empty($block['align'])) {
+    $classes .= ' align' . $block['align'];
+}
+?>
+
+<!-- HTML MArlup with ACF fields -->
