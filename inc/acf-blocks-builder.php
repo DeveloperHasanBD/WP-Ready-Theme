e0a376e7324e1c17819c50561867421373746393
Developer Hasan <67751842+DeveloperHasanBD@users.noreply.github.com>
2024-06-20 14:01:15 +0600

    Update acf-blocks-builder.php

--- inc/acf-blocks-builder.php
+++ inc/acf-blocks-builder.php
@@ -3,8 +3,8 @@
 function theme_modules_init() {
 
 	spl_autoload_register( function ( $class_name ) {
-		if ( file_exists( get_template_directory() . '/templates/blocks/' . $class_name . '.php' ) ) {
-			require_once get_template_directory() . '/templates/blocks/' . $class_name . '.php';
+		if ( file_exists( get_template_directory() . '/page-templates/blocks/' . $class_name . '.php' ) ) {
+			require_once get_template_directory() . '/page-templates/blocks/' . $class_name . '.php';
 		}
 	} );
 
